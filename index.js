59584618c2e7fa4e1588948095e77a17bdec1ca1
Mos <mospro@live.com>
2017-11-01 17:55:28 +0200

    fix port for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const request = require('request')
 const bodyParser = require('body-parser')
 const app = express().use(bodyParser.json())
-let port = 80
+let port = process.env.PORT || 80
 
 let topics = require('./data/topics.json')
 
